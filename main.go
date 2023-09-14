bb726719b62b2f9dc4f5b2833a8c9ae540d1c357
joshuaoweipadei <oweipadeijoshie@gmail.com>
2023-09-14 19:16:52 -0400

    initiate the class repository

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int = 2
+	fmt.Println(num)
+	echo()
+}
+
+func echo() {
+	// var strg string = "Class 214"
+	strg := "Class 215, Thr"
+	msg := "Go lang Rocks!!"
+	
+	fmt.Println(strg, msg)
+}
